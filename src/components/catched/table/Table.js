c3706c86980ca24091058061a588828320686ebf
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-04-13 23:47:09 -0300

    Se sube ultima versión a prod

--- src/components/catched/table/Table.js
+++ src/components/catched/table/Table.js
@@ -31,119 +31,134 @@ const CatchedTable = ({ listCatched }) => {
     }
   }, [listCatched]);
 
-  if (listCatched) {
-    if (!listCatched?.length && !isLoading) {
-      return (
-        <>
-          <Flex alignItems="center" minH="100vh" justifyContent="center">
-            <Container maxW="container.lg">
-              <div className={style.withOutPkms}>
-                <h1>Aún no has capturado ningún pokemon</h1>
-              </div>
-            </Container>
-          </Flex>
-        </>
-      );
-    }
-  }
+  console.log(listCatched, "a ver");
 
-  return (
-    <Flex alignItems="center" minH="100vh" justifyContent="center">
-      <Container maxW="container.lg" mt={10}>
-        <div className={style.tableContainer}>
-          {isLoading ? (
-            <>
-              <Heading
-                as="h1"
-                size="lg"
-                mb={6}
-                textAlign="center"
-                borderBottom="1px solid burlywood"
-                pb={2}
-                mt={50}
-              >
-                <Box textColor={"rgb(101, 67, 33)"}>My Team</Box>
-              </Heading>
-              <Box textColor={"rgb(101, 67, 33)"}>
-                Total atrapados: {listCatched?.length}
-              </Box>
-              <Flex gap={10} direction={"column"} mt={50}>
-                <Skeleton width={"100%"} height={100} border={20} />
-                <Skeleton width={"100%"} height={100} border={20} />
-                <Skeleton width={"100%"} height={100} border={20} />
-                <Skeleton width={"100%"} height={100} border={20} />
-                <Skeleton width={"100%"} height={100} border={20} />
-                <Skeleton width={"100%"} height={100} border={20} />
-              </Flex>
-            </>
-          ) : (
-            <>
-              <Heading
-                as="h1"
-                size="lg"
-                mb={6}
-                textAlign="center"
-                borderBottom="1px solid burlywood"
-                pb={2}
-                mt={50}
-              >
-                <Box textColor={"rgb(101, 67, 33)"}>My Team</Box>
-              </Heading>
-              <Box textColor={"rgb(101, 67, 33)"}>
-                Total atrapados: {listCatched?.length}
-              </Box>
-              <TableContainer width={`100%`} mt={10}>
-                <Table variant="simple">
-                  <Thead>
-                    <Tr>
-                      <Th>Name</Th>
-                      <Th>Types</Th>
-                      <Th>Image</Th>
-                    </Tr>
-                  </Thead>
-                  <Tbody>
-                    {listCatched?.map((pokemon) => (
-                      <Tr key={pokemon.id}>
-                        <Td>{pokemon.name}</Td>
-                        <Td>
-                          <Stack direction="row">
-                            {pokemon.types.map((type, index) => {
-                              return (
-                                <Badge
-                                  borderRadius={5}
-                                  variant="solid"
-                                  colorScheme={colorTipos(type.type.name)}
-                                  key={index}
-                                  padding={2}
-                                  backgroundColor={colorTipos(type.type.name)}
-                                >
-                                  {type.type.name}
-                                </Badge>
-                              );
-                            })}
-                          </Stack>
-                        </Td>
-                        <Td>
-                          <Image
-                            height={100}
-                            width={100}
-                            src={
-                              pokemon.image ? pokemon?.image : "/pokebola.jpg"
-                            }
-                            alt={pokemon.name}
-                          />
-                        </Td>
+  if (!listCatched?.length && !isLoading) {
+    return (
+      <>
+        <Flex alignItems="center" minH="100vh" justifyContent="center">
+          <Container maxW="container.lg">
+            <div className={style.withOutPkms}>
+              <h1>Aún no has capturado ningún pokemon</h1>
+            </div>
+          </Container>
+        </Flex>
+      </>
+    );
+  }
+  if (listCatched?.length) {
+    return (
+      <Flex alignItems="center" minH="100vh" justifyContent="center">
+        <Container maxW="container.lg" mt={10}>
+          <div className={style.tableContainer}>
+            {isLoading ? (
+              <>
+                <Heading
+                  as="h1"
+                  size="lg"
+                  mb={6}
+                  textAlign="center"
+                  borderBottom="1px solid burlywood"
+                  pb={2}
+                  mt={50}
+                >
+                  <Box textColor={"rgb(101, 67, 33)"}>My Team</Box>
+                </Heading>
+                <Box textColor={"rgb(101, 67, 33)"}>
+                  Total atrapados: {listCatched?.length}
+                </Box>
+                <Flex gap={10} direction={"column"} mt={50}>
+                  <Skeleton width={"100%"} height={100} border={20} />
+                  <Skeleton width={"100%"} height={100} border={20} />
+                  <Skeleton width={"100%"} height={100} border={20} />
+                  <Skeleton width={"100%"} height={100} border={20} />
+                  <Skeleton width={"100%"} height={100} border={20} />
+                  <Skeleton width={"100%"} height={100} border={20} />
+                </Flex>
+              </>
+            ) : (
+              <>
+                <Heading
+                  as="h1"
+                  size="lg"
+                  mb={6}
+                  textAlign="center"
+                  borderBottom="1px solid burlywood"
+                  pb={2}
+                  mt={50}
+                >
+                  <Box textColor={"rgb(101, 67, 33)"}>My Team</Box>
+                </Heading>
+                <Box textColor={"rgb(101, 67, 33)"}>
+                  Total atrapados: {listCatched?.length}
+                </Box>
+                <TableContainer width={`100%`} mt={10}>
+                  <Table variant="simple">
+                    <Thead>
+                      <Tr>
+                        <Th>Name</Th>
+                        <Th>Types</Th>
+                        <Th>Image</Th>
                       </Tr>
-                    ))}
-                  </Tbody>
-                </Table>
-              </TableContainer>
-            </>
-          )}
-        </div>
-      </Container>
-    </Flex>
-  );
+                    </Thead>
+                    <Tbody>
+                      {listCatched?.map((pokemon) => (
+                        <Tr key={pokemon.id}>
+                          <Td>{pokemon.name}</Td>
+                          <Td>
+                            <Stack direction="row">
+                              {pokemon.types.map((type, index) => {
+                                return (
+                                  <Badge
+                                    borderRadius={5}
+                                    variant="solid"
+                                    colorScheme={colorTipos(type.type.name)}
+                                    key={index}
+                                    padding={2}
+                                    backgroundColor={colorTipos(type.type.name)}
+                                  >
+                                    {type.type.name}
+                                  </Badge>
+                                );
+                              })}
+                            </Stack>
+                          </Td>
+                          <Td>
+                            <Image
+                              height={100}
+                              width={100}
+                              src={
+                                pokemon.image ? pokemon?.image : "/pokebola.jpg"
+                              }
+                              alt={pokemon.name}
+                            />
+                          </Td>
+                        </Tr>
+                      ))}
+                    </Tbody>
+                  </Table>
+                </TableContainer>
+              </>
+            )}
+          </div>
+        </Container>
+      </Flex>
+    );
+  } else {
+    return (
+      <Flex
+        gap={10}
+        direction={"column"}
+        h={"100vh"}
+        w={"50vw"}
+        justifyContent={"center"}
+        alignItems={"center"}
+        margin={"auto"}
+      >
+        <Skeleton height={"50vh"} width={"90vw"} border={20} />
+      </Flex>
+    );
+  }
 };
 
 export default CatchedTable;
