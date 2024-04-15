b0698df42b4e997fba546d87239db99c42042752
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-04-15 14:57:12 -0300

    Se fixea la tabla de singlePokemonPage

--- src/components/loader/LoadingDashboard.js
+++ src/components/loader/LoadingDashboard.js
@@ -5,13 +5,9 @@ import {
   SimpleGrid,
   useMediaQuery,
 } from "@chakra-ui/react";
-import SearchBar from "../searchBar/SearchBar";
 import MyLoader from "./MyLoader";
-import CustomPagination from "../Pagination/Pagination";
 
 const LoadingDashboard = () => {
-  const [isLargerThan766] = useMediaQuery("(min-width: 766px)");
-
   return Array.from({ length: 20 }).map((_, index) => (
     <div key={new Date().getTime() + index}>
       <MyLoader />
