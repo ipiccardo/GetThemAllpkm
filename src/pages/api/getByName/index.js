171963c6baf11f3ab431913947595a4a4205e0dd
ipiccard <ivan.piccardo@tsoftlatam.com>
2024-04-23 16:45:08 -0300

    Se comienza a hacer el filter por name

--- src/pages/api/getByName/index.js
+++ src/pages/api/getByName/index.js
@@ -4,15 +4,23 @@ export default async function handler(req, res) {
   const { name } = req.query;
 
   try {
-    const response = await axios.get(
-      `https://pokeapi.co/api/v2/pokemon/${name
-        .toLowerCase()
-        .replace(/\s+/g, "-")}`
+    const data = await axios.get(`https://pokeapi.co/api/v2/pokemon`);
+
+    const filteredPokemon = data.data.results.filter((pokemon) =>
+      pokemon.name.includes(name.toLowerCase().replace(/\s+/g, "-"))
     );
-    const pokemon = response.data;
-    res.status(200).json(pokemon);
+
+    const gettingPokeData = filteredPokemon.map((pokemon) => {
+      return axios.get(pokemon.url);
+    });
+
+    const allPokeData = await Promise.all(gettingPokeData);
+
+    const formattedData = allPokeData.map((response) => response.data);
+
+    res.status(200).json(formattedData);
   } catch (error) {
-    if (error.response.data === "Not Found") {
+    if (error.response && error.response.status === 404) {
       res.status(404).json({ message: "El Pokémon no existe" });
     } else {
       console.error("Error fetching Pokémon:", error);
@@ -20,3 +28,21 @@ export default async function handler(req, res) {
     }
   }
 }
+
+//   try {
+//     const response = await axios.get(
+//       `https://pokeapi.co/api/v2/pokemon/${name
+//         .toLowerCase()
+//         .replace(/\s+/g, "-")}`
+//     );
+//     const pokemon = response.data;
+//     res.status(200).json(pokemon);
+//   } catch (error) {
+//     if (error.response.data === "Not Found") {
+//       res.status(404).json({ message: "El Pokémon no existe" });
+//     } else {
+//       console.error("Error fetching Pokémon:", error);
+//       res.status(500).json({ message: "Hubo un error al buscar el Pokémon" });
+//     }
+//   }
+// }
