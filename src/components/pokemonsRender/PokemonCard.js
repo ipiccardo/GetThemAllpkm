b7214b1e9ba773a2a50589c78707dce3d1307517
Iván Piccardo <piccardo.ivan@gmail.com>
2024-06-20 16:32:24 -0300

    Se modifica el render condicional de las imagenes

--- src/components/pokemonsRender/PokemonCard.js
+++ src/components/pokemonsRender/PokemonCard.js
@@ -8,39 +8,8 @@ import {
   Skeleton,
 } from "@chakra-ui/react";
 import { colorTipos } from "@/utils/colorTypes";
-import { useState, useEffect } from "react";
-import styles from "./pokemonrender.module.css";
 
 export default function PokemonCard({ pokemon, singlePokemon }) {
-  const [imageUrl, setImageUrl] = useState(null);
-
-  const [isLoading, setIsLoading] = useState(true);
-
-  useEffect(() => {
-    const fetchImage = async () => {
-      try {
-        const response = await fetch(
-          `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png`
-        );
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch image");
-        }
-
-        setImageUrl(response.url);
-        setIsLoading(false);
-      } catch (error) {
-        console.error("Error fetching image:", error);
-        setImageUrl(
-          `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
-        );
-        setIsLoading(false);
-      }
-    };
-
-    fetchImage();
-  }, [pokemon.id]);
-
   if (singlePokemon) {
     return (
       <Stack
@@ -75,6 +44,17 @@ export default function PokemonCard({ pokemon, singlePokemon }) {
     );
   }
 
+  const pokemonWithoutPic = [
+    "10080",
+    "10081",
+    "10082",
+    "10083",
+    "10084",
+    "10085",
+    "10061",
+    "10158",
+  ];
+
   return (
     <Stack
       spacing="5"
@@ -85,11 +65,16 @@ export default function PokemonCard({ pokemon, singlePokemon }) {
       alignItems="center"
     >
       <AspectRatio w="full" ratio={1}>
-        {isLoading ? (
-          <div style={{ width: "100%", height: "100%" }} />
-        ) : (
-          <Image objectFit="contain" src={imageUrl} alt={`${pokemon.name}`} />
-        )}
+        <Image
+          src={
+            pokemonWithoutPic.some(
+              (id) => id.toString() === pokemon.id.toString()
+            )
+              ? `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`
+              : `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/home/${pokemon.id}.png`
+          }
+          alt={`${pokemon.name}`}
+        />
       </AspectRatio>
       <Text textAlign="center" textTransform="Capitalize">
         {pokemon.name}
